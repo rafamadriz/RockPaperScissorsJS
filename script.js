7a198fff0e611b62fcf2971c91079a3a7cb947ec
Rafael Madriz <rafa@rafaelmadriz.com>
2022-07-17 22:14:29 -0300

    Refactor game logic to start working on UI
    Setting up the ground to start working on UI, so users can play game with
    buttons in a webpage instead of the console.

--- script.js
+++ script.js
@@ -1,65 +1,44 @@
-const gameElements = ['rock', 'paper', 'scissors'];
+const gameElements = ["rock", "paper", "scissors"];
 
-const M = Math;
-const randomNumber = (min, max) => M.floor(M.random() * (max - min) + min);
+const randomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min) + min);
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
 
-const winner = { isPlayerWinner: false, isComputerWinner: false };
-const chooseWinnter = (player, computer) => {
-  if (
-    (player === gameElements[0] && computer == gameElements[1]) ||
-    (player === gameElements[1] && computer == gameElements[2]) ||
-    (player === gameElements[2] && computer == gameElements[0])
-  ) {
-    winner.isPlayerWinner = false;
-    winner.isComputerWinner = true;
+const whoIsWinner = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    return "tie";
   } else if (
-    (player === gameElements[0] && computer == gameElements[2]) ||
-    (player === gameElements[1] && computer == gameElements[0]) ||
-    (player === gameElements[2] && computer == gameElements[1])
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    winner.isPlayerWinner = true;
-    winner.isComputerWinner = false;
+    return "player";
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    return "computer";
   }
 };
 
-const playRound = (playerSelection, computerSelection) => {
-  chooseWinnter(playerSelection, computerSelection);
-
-  if (winner.isPlayerWinner && !winner.isComputerWinner) {
-    console.log(
-      `You win the round! ${playerSelection} beats ${computerSelection}`
-    );
-  } else if (!winner.isPlayerWinner && winner.isComputerWinner) {
-    console.log(
-      `You lose the round! ${computerSelection} beats ${playerSelection}`
-    );
-  } else if (!winner.isPlayerWinner && !winner.isComputerWinner) {
-    console.log(`It's a tie!`);
+const playRound = (string) => {
+  if (string === "tie") {
+    return;
+  } else if (string === "player") {
+    playerScore++;
+  } else if (string === "computer") {
+    computerScore++;
   }
 };
 
-const game = () => {
-  let playerCount = 0;
-  let computerCount = 0;
-  let count = 0;
-
-  while (count < 5) {
+document.querySelectorAll(".btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const playerSelection = button.value;
     const computerSelection = gameElements[randomNumber(0, 3)];
-    const playerSelection = prompt("What's your play").toLowerCase();
-
-    playRound(playerSelection, computerSelection);
-
-    if (winner.isPlayerWinner) playerCount++;
-    if (winner.isComputerWinner) computerCount++;
-
-    console.log(`Round: ${count + 1}`);
-    count++;
-  }
-
-  if (playerCount > computerCount)
-    console.log(`You've won the game! Congratulations`);
-  if (computerCount > playerCount) console.log(`You've lost the game :(`);
-  console.log(`Player Score: ${playerCount}\nComputer Score: ${computerCount}`);
-};
-
-game();
+    playRound(whoIsWinner(playerSelection, computerSelection));
+    roundCount++;
+  });
+});
